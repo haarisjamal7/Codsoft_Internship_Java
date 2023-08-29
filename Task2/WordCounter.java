acc8126e7861c8077fa4acf8262d70f1009d33e0
haarisjamal7 <116809905+haarisjamal7@users.noreply.github.com>
2023-08-29 14:44:18 +0530

    WordCounter.java

--- Task2/WordCounter.java
+++ Task2/WordCounter.java
@@ -1,35 +1,57 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class WordCounter {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputText = "";
 
-        System.out.print("Enter a sentence: ");
-        String input = scanner.nextLine();
+        try {
+            System.out.println("Choose an option:");
+            System.out.println("1. Enter a text");
+            System.out.println("2. Provide a file");
 
-        int wordCount = countWords(input);
-        String[] words = splitIntoWords(input);
+            int choice = Integer.parseInt(reader.readLine());
 
-        System.out.println("Total number of words: " + wordCount);
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter a text: ");
+                    inputText = reader.readLine();
+                    break;
+                case 2:
+                    System.out.print("Enter the file path: ");
+                    String filePath = reader.readLine();
+                    inputText = readFile(filePath);
+                    break;
+                default:
+                    System.out.println("Invalid option. Exiting.");
+                    return;
+            }
 
-        scanner.close();
-
+            int wordCount = countWords(inputText);
+            System.out.println("Total words in the text: " + wordCount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static int countWords(String input) {
-        if (input == null || input.isEmpty()) {
-            return 0;
+    private static String readFile(String filePath) throws IOException {
+        StringBuilder content = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            content.append(line).append("\n");
         }
 
-        String[] words = input.split("[\\s\\p{Punct}]+");
+        reader.close();
+        return content.toString();
+    }
+
+    private static int countWords(String text) {
+        String[] words = text.split("[\\s\\p{Punct}]+"); // Split by spaces or punctuation
         return words.length;
-    }
-
-    public static String[] splitIntoWords(String input) {
-        if (input == null || input.isEmpty()) {
-            return new String[0];
-        }
-
-        return input.split("[\\s\\p{Punct}]+");
     }
 }
